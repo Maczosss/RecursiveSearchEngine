d991d179ec712fb2328903fbd672a1c8698cc2f2
Maczosss <maciej.jaremowicz@gmail.com>
2019-12-10 21:24:41 +0100

    making code faster

--- RecoursiveSearchEngine/src/com/company/Main.java
+++ RecoursiveSearchEngine/src/com/company/Main.java
@@ -4,9 +4,9 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        String fileName = "path to file";
+        String fileName = "file path";
         Reader reader = new Reader(fileName);
         MapSaver saver = new MapSaver(reader.getTextFromFiles());
-        System.out.println(saver.getMapWithAllData());
+        System.out.println(saver.getMapWithAllData2());
     }
 }
