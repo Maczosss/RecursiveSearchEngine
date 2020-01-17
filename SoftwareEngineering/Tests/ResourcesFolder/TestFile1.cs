e00495452e7dd7d970b38d15cbe471dbdf4221cb
Michal_Piec <michalbuczek00@gmail.com>
2020-01-17 20:29:07 +0100

    Unit test for net files finder added.

---
+++ SoftwareEngineering/Tests/ResourcesFolder/TestFile1.cs
@@ -0,0 +1,36 @@
+﻿/*
+using TestUsing;
+
+//[Test]
+namespace TestNamespace
+{
+    class TestClass1
+    {
+        public List<string> CsFiles { get; set; }
+        public Ctor(List<string> csFiles)
+        {
+            this.CsFiles = csFiles;
+        }
+
+        public List<ClassesDefinedInFile> TestMethodDeclaration()
+        {
+            
+            reader.ReadLine()
+                
+
+            
+            return definitionsInFile;
+        }
+        
+        public List<ClassUsagesFoundInFile> OtherTestDeclaration(param param)
+        {
+            
+            foreach (var variable in collection)
+            {
+               testMethod3();
+            }
+            return result;
+        }
+    }
+}
+*/
