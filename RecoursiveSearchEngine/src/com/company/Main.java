35c85a0fc82ec22b2d7aaa658bb9d10184af678a
Maczosss <maciej.jaremowicz@gmail.com>
2020-02-11 22:19:43 +0100

    [Reforged] Historia 1, potrzebne zmiany

--- RecoursiveSearchEngine/src/com/company/Main.java
+++ RecoursiveSearchEngine/src/com/company/Main.java
@@ -9,32 +9,36 @@ public class Main {
         String fileName = System.getProperty("user.dir")+"\\RecoursiveSearchEngine\\src\\com\\company";
         System.out.println(fileName);
         Reader reader = new Reader(fileName);
-        MapImageGenerator generator;
+        reader.getThroughFiles(fileName);
+        reader.show();
+        reader.trimMapToNewOneWithStrings();
 
-        MethodCounter methodCounter = new MethodCounter(reader.getTextFromFiles());
-        System.out.println(methodCounter.getMethodMap());
-        methodCounter.getMethodsForClasses();
-        System.out.println("=========================");
-        methodCounter.show();
+       // MapImageGenerator generator;
 
-        generator=new MapImageGenerator("test2",methodCounter.getMethodsInClassMap(),methodCounter.getMethodCounter());
-        generator.toPNG();
+//        MethodCounter methodCounter = new MethodCounter(reader.getTextFromFiles());
+//        System.out.println(methodCounter.getMethodMap());
+//        methodCounter.getMethodsForClasses();
+//        System.out.println("=========================");
+//        methodCounter.show();
 
-
-        MapSaver saver = new MapSaver(reader.getTextFromFiles());
-        Map<String, List<String>> neighbourMap = saver.getMapWithAllData();
-
-        System.out.println(neighbourMap);
-        generator = new MapImageGenerator("test",neighbourMap);
-        generator.toPNG();
-
-
-
-        String[]pngsPath=new String [3];
-        pngsPath[0]="test.png";             //do podmiany nazwy png przekazywane do Frame jako sciezki do pliku
-        pngsPath[1]="test2.png";
-        pngsPath[2]="test.png";
-
-        new Frame(pngsPath);
+//        generator=new MapImageGenerator("test2",methodCounter.getMethodsInClassMap(),methodCounter.getMethodCounter());
+//        generator.toPNG();
+//
+//
+//        MapSaver saver = new MapSaver(reader.getTextFromFiles());
+//        Map<String, List<String>> neighbourMap = saver.getMapWithAllData();
+//
+//        System.out.println(neighbourMap);
+//        generator = new MapImageGenerator("test",neighbourMap);
+//        generator.toPNG();
+//
+//
+//
+//        String[]pngsPath=new String [3];
+//        pngsPath[0]="test.png";             //do podmiany nazwy png przekazywane do Frame jako sciezki do pliku
+//        pngsPath[1]="test2.png";
+//        pngsPath[2]="test.png";
+//
+//        new Frame(pngsPath);
     }
 }
--- RecoursiveSearchEngine/src/com/company/Reader.java
+++ RecoursiveSearchEngine/src/com/company/Reader.java
@@ -4,10 +4,7 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Objects;
+import java.util.*;
 import java.util.stream.Collectors;
 
 class Reader {
@@ -16,12 +13,105 @@ class Reader {
     private BufferedReader reader;
     private List<String> lines = new ArrayList<>();
     private List<String> results = new ArrayList<>();
+    private Map<String, List<String>> filesCollection = new HashMap<>(); // <nazwa folderu< nazwa pliku, tresc>>
+    private static Map<File, List<File>> directoriesAndFiles = new HashMap<>();
+      
+    private int filter = 0;
 
 
     Reader(String fileName) {
         this.fileName = fileName;
         this.file = new File(fileName);
     }
+
+
+    public void getThroughFiles(String fileName) {
+
+        File root = new File(fileName);
+        File[] list = root.listFiles();
+        List<File> dirs = new LinkedList<>();
+        List<File> files = new LinkedList<>();
+        String temp = root.getPath();
+        System.out.println(temp);
+        this.filter = temp.lastIndexOf("\\");
+
+        if (list == null) return;
+        for (File f : list) {
+            if (f.isDirectory()) {
+                dirs.add(f);
+            } else {
+                if (f.toString().contains(".java"))
+                    files.add(f);
+            }
+        }
+        directoriesAndFiles.put(root, files);
+        if (dirs.size() != 0) {
+            for (File dir : dirs) {
+                getThroughFiles(dir);
+            }
+        }
+    }
+
+    public void getThroughFiles(File directory) {
+
+        File[] list = directory.listFiles();
+        List<File> dirs = new LinkedList<>();
+        List<File> files = new LinkedList<>();
+
+        if (list == null) return;
+        for (File f : list) {
+            if (f.isDirectory()) {
+                dirs.add(f);
+            } else {
+                if (f.toString().contains(".java"))
+                    files.add(f);
+            }
+        }
+        directoriesAndFiles.put(directory, files);
+        if (dirs.size() != 0) {
+            for (File dir : dirs) {
+                getThroughFiles(dir);
+            }
+        }
+    }
+
+    public void show() {
+        System.out.println(directoriesAndFiles);
+    }
+
+    public void trimMapToNewOneWithStrings() {
+        for (File f : directoriesAndFiles.keySet()) {
+            List<File> temporary = directoriesAndFiles.get(f);
+            List<String> listOfFilesInDirectory = new LinkedList<>();
+            for (File temp : temporary) {
+                if (temp.getName().length() > filter) {
+                    listOfFilesInDirectory.add(temp.getName().substring(0, filter));
+                } else {
+                    listOfFilesInDirectory.add(temp.getName());
+
+                }
+            }
+            if (f.toString().length() > filter) {
+                String name = f.getPath();
+                name.substring(0, filter);
+                filesCollection.put(name, listOfFilesInDirectory);
+            }
+            else {
+                String name = f.getPath();
+                filesCollection.put(name, listOfFilesInDirectory);
+            }
+
+        }
+
+        System.out.println("================================");
+        System.out.println(filesCollection);
+    }
+
+
+    //reforging ends
+
+    //===================================================
+
 
     List<String> getTextFromFiles() {
         if (file.canRead()) {
